f38bbec1775c51866a6dc2d31ac6708be0a794fd
dgbailey <bailey.dust@gmail.com>
2019-04-09 21:57:00 -0700

    media queries using tagged template literals added

--- todo/src/components/Item.js
+++ todo/src/components/Item.js
@@ -3,16 +3,42 @@ import { connect } from 'react-redux';
 import {additem,togglestatus} from '../actions';
 import styled, { css } from 'styled-components';
 
+//media templates
+
+const sizes = {
+    desktop: 992,
+    tablet: 768,
+    phone: 576,
+}
+
+const media = Object.keys(sizes).reduce((acc,label) => {
+    acc[label] = (...args) => 
+    css`@media (max-width:${sizes[label]}px){
+        ${css(...args)}
+
+    }`
+
+    return acc
+
+    
+},{})
+
+
+
 const StrikeWrap = styled.div`
     text-decoration: ${props => props.strike === 'strike' ? 'line-through':null};
     padding:10px 15px;
     border-radius:5px;
+    ${media.desktop`background:green;`}
     &:hover{
         cursor:pointer;
 
     }
+    
 
-`
+`;
+
+
 
 export const Item = (props)=>{
 
